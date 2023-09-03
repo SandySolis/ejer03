68584376094bb8e4e811af7d9d376f924eec9fe6
Sandy Solis <sandysolis0305@gmail.com>
2023-09-02 21:09:01 -0500

    Agregar archivos de proyecto.

---
+++ ejer03/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese la cantidad de llantas que desea comprar: ");
+    int cantllantas = int.Parse(Console.ReadLine());
+
+    decimal precioporllanta = (cantllantas >= 5) ? 700m : 800m;
+
+    decimal total = cantllantas * precioporllanta;
+
+Console.WriteLine("El total a pagar es: "+ total);
+ 
